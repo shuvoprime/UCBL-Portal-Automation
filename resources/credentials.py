44c134b905446d528ff1e28089e6c3f49c7fb8c0
Rakibul Alam Shuvo <54779883+shuvoprime@users.noreply.github.com>
2022-02-08 02:36:59 +0600

    Update credentials.py

--- resources/credentials.py
+++ resources/credentials.py
@@ -1,11 +1,11 @@
 import random
 
 class Constantinope:
-    username = "portal_superuser"
-    password = "portal_superuser!23"
-    complain_portal = "https://uat-cm.upay.systems/"
-    demo_text = "test_s3"
-    wallet_number = "01717571984"
+    username =     
+    password =
+    complain_portal = "https://uatcm.upay.systems/"
+    demo_text = 
+    wallet_number = 
 
     from_date_day = "01"
     from_date_month = "09"
